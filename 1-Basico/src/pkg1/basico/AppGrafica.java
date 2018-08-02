76c174c7b657a3856801c62543b47338094e51bf
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-01 22:46:57 -0400

    Clase 11: CalculadoraGrafica P1(Declaracion de los metodos)

---
+++ 1-Basico/src/pkg1/basico/AppGrafica.java
@@ -0,0 +1,27 @@
+package pkg1.basico;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class AppGrafica {
+    public AppGrafica(){
+    
+    }
+    private double suma(double x,double y){
+        double suma = x + y;
+        return suma;
+    }
+    private double resta(double x, double y){
+        double resta = x - y;
+        return resta;
+    }
+    private double mult(double x, double y){
+        double mult = x * y;
+        return mult;
+    }
+    private double div(double x,double y){
+        double mult = x / y;
+        return mult;
+    }
+}
