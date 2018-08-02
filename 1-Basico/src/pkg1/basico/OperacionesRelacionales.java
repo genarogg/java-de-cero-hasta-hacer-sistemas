0276c02934b11cd2cbaf98af176f6805537de8b4
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-01 22:00:48 -0400

    Clase 6: Uso de los operadores Realcionales

---
+++ 1-Basico/src/pkg1/basico/OperacionesRelacionales.java
@@ -0,0 +1,14 @@
+package pkg1.basico;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class OperacionesRelacionales {
+    public static void main(String[]args){
+        int x = 0;
+        int y = 2;
+        boolean b = (x!=0) && ((y/x)!= 0);
+        System.out.println(b);
+    }
+}
