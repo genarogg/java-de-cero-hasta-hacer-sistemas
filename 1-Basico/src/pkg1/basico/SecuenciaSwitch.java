0f122efbef4da12167dcae2fcd5b5d45faa79ce4
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-01 22:44:26 -0400

    Clase 10: Uso de la secuenciaSwitch

---
+++ 1-Basico/src/pkg1/basico/SecuenciaSwitch.java
@@ -0,0 +1,47 @@
+package pkg1.basico;
+
+/**
+ *
+ * @author Genarogg
+ */
+
+public class SecuenciaSwitch {
+    public static void main(String[]args){
+        int a = 5;
+        
+        switch(a){
+            case 1:{
+                System.out.println("1");
+                break;
+            }
+            case 2:{
+                System.out.println("2");
+                break;
+            }
+            case 3:{
+                System.out.println("3");
+                break;
+            }
+            case 4:{
+                System.out.println("4");
+                break;
+            }
+            case 5:{
+                System.out.println("5");
+                break;
+            }
+            case 6:{
+                System.out.println("6");
+                break;
+            }
+            case 7:{
+                System.out.println("7");
+                break;
+            }
+            default:{
+                System.out.println("no hay coincidencias");
+                break;
+            }
+        }
+    }
+}
