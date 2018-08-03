d1851236483eea396898d6d0acade47db3818173
Unknown <unknown@example.com>
2018-08-02 22:51:15 -0400

    clase 4: vectores

--- 1-Basico/src/pkg1/basico/Vectores.java
+++ 1-Basico/src/pkg1/basico/Vectores.java
@@ -1,9 +1,22 @@
 package pkg1.basico;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Genarogg
  */
 public class Vectores {
-    
+    public static void main(String[]args){    
+        Scanner s = new Scanner(System.in);
+        int v[] = new int [9];
+        
+        for(int i = 0;i < v.length; i++){
+            int n = s.nextInt();
+            v[i] = n;
+        }  
+        for(int i = 0; i < v.length;i++){
+            System.out.println("V["+ i +"] = " + v[i]);
+        }
+    }
 }
