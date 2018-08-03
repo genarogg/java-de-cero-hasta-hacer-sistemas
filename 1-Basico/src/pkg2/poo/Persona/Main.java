95eec0f212cfb6aa816acb738c003f1e6bd57e9e
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-02 23:07:58 -0400

    Clase 12: objetos P3 (persona)

---
+++ 1-Basico/src/pkg2/poo/Persona/Main.java
@@ -0,0 +1,24 @@
+package pkg2.poo.Persona;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Genarogg
+ */
+
+
+public class Main {
+    public static void main(String[]args){
+        String nombre = JOptionPane.showInputDialog("Dime tu nombre: ");
+        String nacionalidad = JOptionPane.showInputDialog("Dime tu nacionalidad: ");
+        double altura = Double.parseDouble(JOptionPane.showInputDialog("Dime tu altura: "));
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Dime tu edad: "));
+        
+        Persona persona = new Persona(nombre, nacionalidad,  altura, edad);
+        
+        JOptionPane.showMessageDialog(null,"tu nombre es: " + persona.getNombre());
+        JOptionPane.showMessageDialog(null,"tu nacionalidad es: " + persona.getNacionalidad());
+        JOptionPane.showMessageDialog(null,"tu altura es: " + persona.getAlturas());
+        JOptionPane.showMessageDialog(null,"tu edad es: " + persona.getEdads());
+    }    
+}
