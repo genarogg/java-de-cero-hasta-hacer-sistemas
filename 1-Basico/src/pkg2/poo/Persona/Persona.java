5a8413933e6441171eb177d576836126716d6795
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-02 23:05:51 -0400

    Clase 12: objetos P1 (persona)

---
+++ 1-Basico/src/pkg2/poo/Persona/Persona.java
@@ -0,0 +1,19 @@
+package pkg2.poo.Persona;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Persona {
+    private String nombre;
+    private String nacionalidad;
+    private double alturas;
+    private int edads;
+
+    public Persona(String nombre, String nacionalidad, double alturas, int edads) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.alturas = alturas;
+        this.edads = edads;
+    }
+}
