8116575303ef3e989517d59d436d929df38d2588
Unknown <unknown@example.com>
2018-08-02 23:06:34 -0400

    Clase 12: objetso P2  (persona)

--- 1-Basico/src/pkg2/poo/Persona/Persona.java
+++ 1-Basico/src/pkg2/poo/Persona/Persona.java
@@ -16,4 +16,41 @@ public class Persona {
         this.alturas = alturas;
         this.edads = edads;
     }
+    
+    public int getEdads() {
+        return edads;
+    }
+
+    public void setEdads(int edads) {
+        this.edads = edads;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public double getAlturas() {
+        return alturas;
+    }
+
+    public void setAlturas(double alturas) {
+        this.alturas = alturas;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", nacionalidad=" + nacionalidad + ", alturas=" + alturas + ", edads=" + edads + '}';
+    }
 }
