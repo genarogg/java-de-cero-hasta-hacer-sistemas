e34c7b2d033576f20c88a740f0586596792742ac
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-06 00:19:01 -0400

    Inicio del proyecto de PVZ Con animaciones

---
+++ 1-Basico/src/pkg3Herencia/PVZproyect/Main/Window.java
@@ -0,0 +1,22 @@
+package pkg3Herencia.PVZproyect.Main;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Window {
+     
+    public Window(){
+        
+        JFrame ventana = new JFrame();
+        ventana.add(new VisualManager());
+        ventana.setSize(1000, 1000);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        Window ventana = new Window();
+    }
