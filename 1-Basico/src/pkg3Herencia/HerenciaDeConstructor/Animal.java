abdd5765103360c784f08aa126a51fefbae8d166
Genarogg <gena_gonzalez15@hotmail.com>
2018-08-05 23:38:28 -0400

    Fin de Herencia del constructor

--- 1-Basico/src/pkg3Herencia/HerenciaDeConstructor/Animal.java
+++ 1-Basico/src/pkg3Herencia/HerenciaDeConstructor/Animal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg3Herencia.HerenciaDeConstructor;
 
 /**
@@ -10,5 +5,7 @@ package pkg3Herencia.HerenciaDeConstructor;
  * @author Genarogg
  */
 public class Animal {
-    
+    public Animal(){
+        System.out.println("Constructor Animal");
+    }
 }
