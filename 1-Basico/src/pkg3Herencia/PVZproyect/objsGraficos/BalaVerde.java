6e9982a0d32454d1f7459e8d0a7ccf4552619d2f
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-06 23:12:52 -0400

    +Class: balaVere,  Zombie

---
+++ 1-Basico/src/pkg3Herencia/PVZproyect/objsGraficos/BalaVerde.java
@@ -0,0 +1,28 @@
+package pkg3Herencia.PVZproyect.objsGraficos;
+
+import java.awt.Graphics;
+import static javafx.scene.input.KeyCode.I;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Genarogg
+ */
+
+public class BalaVerde extends GraficosSuperClase {
+    public BalaVerde(int x, int y){
+        super(x, y);
+        icon = new ImageIcon(getClass().getResource("/imagenes/BalaVerde.png"));
+        imgGrafica = icon.getImage();
+    }
+    
+    @Override
+    public  void mover (){
+        xCoord += 8;
+    }
+    
+    @Override
+    public void draw(Graphics g){
+        g.drawImage(imgGrafica, xCoord, yCoord, 30, 30, null);
+    }
+}
---
+++ 1-Basico/src/pkg3Herencia/PVZproyect/objsGraficos/Zombie.java
@@ -0,0 +1,28 @@
+package pkg3Herencia.PVZproyect.objsGraficos;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Zombie extends GraficosSuperClase {
+    public Zombie(int x, int y){
+        super(x, y);
+        
+        icon = new ImageIcon(getClass().getResource("/imagenes/Zombie.jpg"));
+        imgGrafica = icon.getImage();
+    }
+    
+    @Override
+    public void mover(){
+     xCoord -= 3;
+    }
+    
+    @Override
+    public void draw(Graphics g){
+       g.drawImage(imgGrafica, xCoord, yCoord, 80, 110, null);
+    }
+}
