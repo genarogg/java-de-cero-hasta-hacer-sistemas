c9790048c7d437d5f5da5bd253cb53b905a25dd8
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-07 19:45:19 -0400

    +Clase: EmpleadoBaseMasComision p2

--- java-de-cero-hasta-hacer-sistemas/src/pkg3Herencia/sistemaDeNomina/EmpleadoBaseMasComision.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg3Herencia/sistemaDeNomina/EmpleadoBaseMasComision.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg3Herencia.sistemaDeNomina;
 
 /**
  *
  * @author Genarogg
  */
-public class EmpleadoBaseMasComision {
+public class EmpleadoBaseMasComision extends EmpleadoPorComision {
     
+    private double salarioBase;
+    
+    public EmpleadoBaseMasComision(String nombre, String apellido, String nss,
+                                   double ventas, double tarifa, double salario){
+        super(nombre, apellido, nss, ventas, tarifa);
+        
+    }
+    
+    public void establecerSalarioBase(double salario){
+        if(salario < 0.0){
+            salario = 0.0;
+        }
+        else{
+            salarioBase = 0.0;
+        }
+    }
+    
+    public double obtenerSalarioBase(){
+        return salarioBase;
+    }
+    
+    @Override
+    public double ingresos(){
+        return obtenerSalarioBase() + super.ingresos();
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s%s; %s: $%,.2f",
+                "con salario base", super.toString(),
+                "salario base", obtenerSalarioBase());    
+    }
 }
