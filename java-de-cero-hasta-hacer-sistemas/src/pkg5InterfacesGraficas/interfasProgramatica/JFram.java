449f7f6e550c030a5db231a41528f210ac01d87b
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-11 14:53:21 -0400

    jFrame -> basico

---
+++ java-de-cero-hasta-hacer-sistemas/src/pkg5InterfacesGraficas/interfasProgramatica/JFram.java
@@ -0,0 +1,23 @@
+package interfasProgramatica;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JFram  {
+    public JFram(){
+        JLabel etiqueta = new JLabel();
+        JFrame f = new JFrame();
+        
+        etiqueta.setBounds(200,200,50,20);
+        etiqueta.setIcon(new ImageIcon("1.jpg"));
+        
+        f.add(etiqueta);
+        f.setVisible(true);        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+    }
+    public static void main(String[]args){
+        JFram f =new JFram();
+    }
+}
