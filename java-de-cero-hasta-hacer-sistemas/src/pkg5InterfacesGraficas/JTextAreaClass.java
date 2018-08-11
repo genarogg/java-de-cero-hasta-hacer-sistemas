f168faa4767ab3926e1792801066c49930b857e7
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-11 15:48:02 -0400

    JTextAreaClass

---
+++ java-de-cero-hasta-hacer-sistemas/src/pkg5InterfacesGraficas/JTextAreaClass.java
@@ -0,0 +1,33 @@
+package JTextArea;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class JTextAreaClass {
+    public JTextAreaClass(){
+        JFrame jF = new JFrame();
+        JTextArea jTA = new JTextArea();
+        
+        jTA.setText("\nhola ");
+        jTA.append("mundo");
+        
+       
+        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        int ancho = pantalla.width;
+        int alto = pantalla.height;
+        
+        jTA.setBounds(10,10,300,300);
+        jF.setBounds(ancho/3,alto/4,350,350);
+        
+        jF.add(jTA);
+        jF.setLayout(null);
+        jF.setVisible(true);
+        jF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public static void main(String[] args) {
+        JTextAreaClass d = new JTextAreaClass();
+    }
+}
