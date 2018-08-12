10ac979a46f2f26e7a5decba2d9fa58c42ce97b0
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-12 16:21:33 -0400

    JToggleButtonClass

---
+++ java-de-cero-hasta-hacer-sistemas/src/pkg5InterfacesGraficas/JToggleButtonClass.java
@@ -0,0 +1,33 @@
+package pkg5InterfacesGraficas;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JToggleButton;
+
+public class JToggleButtonClass {
+    
+    public JToggleButtonClass(){
+        JFrame jF = new JFrame();
+        JToggleButton jTB = new JToggleButton("go",true);
+        
+        
+        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        int ancho = pantalla.width;
+        int alto = pantalla.height;
+        
+        jF.setBounds(ancho/3,alto/4,300,120);
+        jTB.setBounds(100,40,50,40);
+        
+        System.out.println("el togle esta " +  jTB.isEnabled());
+        
+        jF.add(jTB);
+        jF.setLayout(null);
+        jF.setVisible(true);
+        jF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        new JToggleButtonClass();
+    }
+}
