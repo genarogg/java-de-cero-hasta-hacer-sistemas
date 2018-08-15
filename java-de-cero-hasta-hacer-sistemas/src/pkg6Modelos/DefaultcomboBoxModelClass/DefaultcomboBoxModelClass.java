6fa11feaaae8ba74c6dfed709608eccc69bb2211
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-14 23:57:03 -0400

    DefaultcomboBoxModelClass p2

--- java-de-cero-hasta-hacer-sistemas/src/pkg6Modelos/DefaultcomboBoxModelClass/DefaultcomboBoxModelClass.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg6Modelos/DefaultcomboBoxModelClass/DefaultcomboBoxModelClass.java
@@ -32,7 +32,33 @@ public class DefaultcomboBoxModelClass {
         p2.setNacionlidad("Venezolano");
         p2.setEdad(45);
         
+        p3.setNombre("Gabriel");
+        p3.setDereccion("San Francisco de Asis");
+        p3.setNacionlidad("Venezolano");
+        p3.setEdad(20);
         
+        p4.setNombre("Hilse");
+        p4.setDereccion("San Francisco de Asis");
+        p4.setNacionlidad("Venezolano");
+        p4.setEdad(64);
+        
+        dCBM.addElement(p1);
+        dCBM.addElement(p2);
+        dCBM.addElement(p3);
+        dCBM.addElement(p4);
+        
+        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = pantalla.width;
+        int y = pantalla.height;
+        
+        jF.setBounds(x/3, y/4,300,300);
+        jCB.setBounds(10, 10, 100, 30);
+        
+        jF.add(jCB);
+        
+        jF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jF.setLayout(null);
+        jF.setVisible(true);
         
     }
 
