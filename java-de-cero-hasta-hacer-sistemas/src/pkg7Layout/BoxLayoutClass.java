5555fbdf529348a9817c538de518f293d458caa6
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-17 17:35:06 -0400

    BoxLayoutClass p1

---
+++ java-de-cero-hasta-hacer-sistemas/src/pkg7Layout/BoxLayoutClass.java
@@ -0,0 +1,13 @@
+package pkg7Layout;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class BoxLayoutClass {
+    private JPanel panelSuperior, panelMedio,panelInferior;
+    private JLabel nombre, contraseña,titulo;
+    private JTextField campoUsuario;
+    private JButton botonAceptar, botonCancelar;
+    private JPasswordField campoContraseña;
+}
