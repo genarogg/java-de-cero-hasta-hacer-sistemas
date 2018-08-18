d19fb476276d2a504c0f985503aea90bb24bc05e
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-17 20:43:42 -0400

    construccionDePanelSuperior

--- java-de-cero-hasta-hacer-sistemas/src/pkg7Layout/BoxLayoutClass.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg7Layout/BoxLayoutClass.java
@@ -10,4 +10,13 @@ public class BoxLayoutClass {
     private JTextField campoUsuario;
     private JButton botonAceptar, botonCancelar;
     private JPasswordField campoContraseña;
+    
+    public void construccionDePanelSuperior(){
+        panelSuperior = new JPanel();
+        nombre = new JLabel("Nombre de usuario:");
+        campoUsuario = new JTextField(10);
+        panelSuperior.setLayout(new FlowLayout());
+        panelSuperior.add(nombre);
+        panelSuperior.add(campoUsuario);
+    }
 }
