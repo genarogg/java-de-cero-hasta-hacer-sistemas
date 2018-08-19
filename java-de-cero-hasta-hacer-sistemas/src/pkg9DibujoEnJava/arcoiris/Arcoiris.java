065f69235f542b7c267c5b11cf43a679720a8910
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-19 11:05:56 -0400

    Arcoiris p2

--- java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/arcoiris/Arcoiris.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/arcoiris/Arcoiris.java
@@ -1,9 +1,22 @@
 package pkg9DibujoEnJava.arcoiris;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Genarogg
  */
-public class Arcoiris {
+public class Arcoiris extends JPanel {
+    private final static Color VIOLETA = new Color(128, 0, 128);
+    private final static Color INDIGO = new Color(75, 0, 130);
     
+    private Color[] colores = {Color.WHITE, Color.WHITE, VIOLETA, INDIGO,Color.BLUE,
+                               Color.GREEN, Color.YELLOW, Color.ORANGE, Color.RED};
+ 
+    public Arcoiris(){
+        setBackground(Color.WHITE);
+    }
+     
 }
