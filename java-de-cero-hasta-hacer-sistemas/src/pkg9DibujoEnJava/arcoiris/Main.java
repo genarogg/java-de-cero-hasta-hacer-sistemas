2826007711c00c6c8fa0ff87a133bb5a7c48d453
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-19 11:04:57 -0400

    Main de arcoiris

--- java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/arcoiris/Main.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/arcoiris/Main.java
@@ -1,9 +1,20 @@
 package pkg9DibujoEnJava.arcoiris;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Genarogg
  */
+
 public class Main {
-    
+    public static void main(String[]args){
+        JFrame ventana = new JFrame ();
+        Arcoiris arcoiri = new Arcoiris();
+        
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.add(arcoiri);
+        ventana.setSize(400,250);
+        ventana.setVisible(true);
+    }    
 }
