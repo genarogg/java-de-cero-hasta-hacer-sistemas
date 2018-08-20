0459b744b3094d65865767bed533f320f191e41d
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-20 11:09:19 -0400

    Figuras: Case 1

--- java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/rectangulosYOvalos/Figuras.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/rectangulosYOvalos/Figuras.java
@@ -14,5 +14,22 @@ public class Figuras extends JPanel {
     public Figuras(int opcionUsuario){
         opcion = opcionUsuario;
     }
-    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+        for(int i = 0;i < 30;i ++){
+            switch(opcion){
+                case 1:{
+                    int xRect = 10 + i * 10;
+                    int yRect = 10 + i * 10;
+                    int anchoRect = 50 + i * 10;
+                    int altoRect = 50 + i * 10;
+                    g.drawRect(xRect, yRect, anchoRect, altoRect);
+                    break;
+                }
+                
+            }
+        }
+    }
 }
