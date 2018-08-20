dd4354c731b70f94b9198ef2313928ae73b1f946
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-20 11:08:50 -0400

    Figuras: RectangulosYOvalos p2

---
+++ java-de-cero-hasta-hacer-sistemas/src/pkg9DibujoEnJava/rectangulosYOvalos/Figuras.java
@@ -0,0 +1,18 @@
+package pkg9DibujoEnJava.rectangulosYOvalos;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Figuras extends JPanel {
+    
+    private int opcion;
+    
+    public Figuras(int opcionUsuario){
+        opcion = opcionUsuario;
+    }
+    
+}
