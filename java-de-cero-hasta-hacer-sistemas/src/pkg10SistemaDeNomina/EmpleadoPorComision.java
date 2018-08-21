efbeac797ec7d4a1506a0f0cd1ec5516bf7ffa74
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-21 11:19:38 -0400

    EmpleadoPorComision p2

--- java-de-cero-hasta-hacer-sistemas/src/pkg10SistemaDeNomina/EmpleadoPorComision.java
+++ java-de-cero-hasta-hacer-sistemas/src/pkg10SistemaDeNomina/EmpleadoPorComision.java
@@ -10,5 +10,38 @@ package pkg10SistemaDeNomina;
  * @author Genarogg
  */
 public class EmpleadoPorComision {
+    private double ventasBrutas;
+    private double tarifaComision;
     
+    public EmpleadoPorComision(String nombre, String apellido, 
+                               String nss, double ventas,double tarifa){
+       
+        super(nombre, apellido, nss);
+        
+        establecerVentasBrutas(ventas);
+        establecerTarifaComicion(tarifa);
+    }
+    public void establecerTarifaComicion(double tarifa){
+        if(tarifa > 0.0 && tarifa < 1.0){
+            tarifaComision = tarifa;
+        }
+        else{
+            tarifa = 0.0;
+        }
+    }
+    
+    public double obtenerTarifaComision(){
+        return tarifaComision;
+    }
+    public void establecerVentasBrutas(double ventas){
+        if(ventas < 0.0){
+            ventasBrutas = 0.0;
+        }
+        else{
+            ventasBrutas = ventas;
+        }
+    }
+    public double obtenerVentasBrutas(){
+        return ventasBrutas;
+    }
 }
